1b02dd94d5c32ffc54fbfca95beded77edd9ab86
sara <saradernouni@gmail.com>
2024-07-18 09:18:12 +0400

    try1

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+console.log("I like pizza");
+
+
+//window.alert("this is an alert");
+//window.alert("I like Pizza");
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like Pizza!";
