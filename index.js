1df50ab078aa08175cf6f60ca2bc0531d92d8907
sara <saradernouni@gmail.com>
2024-08-10 10:00:44 +0400

    108

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //start from here
 
-let candidates = [
+/* let candidates = [
     { name: "Firealem", age: 70},
     { name: "Abel", age: 46},
     { name: "Sara", age: 25},
@@ -18,4 +18,30 @@ for (let i=0; i<candidates.length; i++){
         console.log(`dear ${candidates[i].name} you are too old`);
 
     }
+} */
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (par - 3 == strokes ) {
+    return names[0];
+  } else if (strokes - par === 1) {
+    return names[1]
+  } else if (strokes - par === 1) {
+    return names[2]
+  } else if (strokes - par === 1) {
+    return names[3]
+  } else if (strokes - par === 1) {
+    return names[4]
+  } else if (strokes - par === 1) {
+    return names[5]
+  } else {
+    return names[6]
+  }
 }
+  // Only change code above this line
+
+
+golfScore(4, 1);
+console.log("im a girl")
